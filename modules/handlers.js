46022b35a8e55ce59c6afc39d0c5dd1f55914c2d
christopher lopez <christopherwlopez89@gmail.com>
2022-10-25 21:21:31 -0700

    created backget for POST

--- modules/handlers.js
+++ modules/handlers.js
@@ -1,16 +1,28 @@
 'use strict';
 
 const Book = require('../models/book');
-
-const getBooks = async (request, response, next) => {
+const Handler = {};
+ Handler.getBooks = async (request, response, next) => {
   try {
     // if I pass in an empty object, that tells Mongoose to get ALL the documents from the database
     const books = await Book.find({});
     response.status(200).send(books);
   } catch(error) {
-    console.error(error);
+    error.customMessage = 'Something went wrong when getting your book';
+    console.error(error.customMessage + error);
     next(error);
   }
 };
 
-module.exports = getBooks;
+Handler.createBook = async (request, response, next) => {
+  try {
+    // if I pass in an empty object, that tells Mongoose to get ALL the documents from the database
+    const book = await Book.create(request.body);
+    response.status(200).send(book);
+  } catch(error) {
+    error.customMessage = 'Something went wrong when creating your book';
+    console.error(error.customMessage + error);
+    next(error);
+  }
+};
+module.exports = Handler;
