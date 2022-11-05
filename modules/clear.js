0fc1015bcb38b73d953f79252acb09ecd266b822
christopher lopez <christopherwlopez89@gmail.com>
2022-11-05 15:26:02 -0700

    started work

---
+++ modules/clear.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_CONNECTION);
+
+const Book = require('./models/book.js');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', async _ => {
+  console.log('We\'re connected!');
+  await Book.deleteMany({});
+  db.close();
+});
+
+
+// look into this clear call
+// clear();
